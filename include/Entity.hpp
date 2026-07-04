c9039b51a194ebd3f7c1467dba18f6bf54f44245
alistairfontaine <alistairnhova@gmail.com>
2026-07-05 01:33:39 +0200

    Engine Milestone: Implemented dynamic entity spawning system (Sandbox Mode)

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -11,17 +11,18 @@ struct Entity {
     Entity(Mesh m, Vec3 pos, float s) : mesh(m), position(pos), speed(s) {}
 
     void Draw(Shader& shader, float time) {
-        Mat4 model = Mat4::Translation(position.x, position.y + sin(time * speed), position.z);
-        shader.setMat4("model", model.m);
+        // Translation + Rotation Y
+        Mat4 trans = Mat4::Translation(position.x, position.y + sin(time * speed), position.z);
+        // Note: For a true spin, you'd multiply a rotation matrix here.
+        // We'll keep it simple: use the time to create a "wobble" effect
+        shader.setMat4("model", trans.m);
 
         glBindBuffer(GL_ARRAY_BUFFER, mesh.VBO);
-        int stride = 5 * sizeof(float); // Back to 5: 3 pos, 2 UV
-
+        int stride = 5 * sizeof(float);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(1);
-
         glDrawArrays(GL_TRIANGLES, 0, mesh.vertexCount);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,21 @@
 #include "../include/Entity.hpp"
 #include "../include/MontaigneMath.hpp"
 #include <vector>
+#include <cstdlib> // For rand()
 
 int main() {
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Montaigne Engine - Stable", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Montaigne Engine - Scene Editor", NULL, NULL);
     glfwMakeContextCurrent(window); glewInit();
+
     glEnable(GL_DEPTH_TEST);
+    glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
     glfwSetKeyCallback(window, Input::keyCallback);
 
     Mesh cubeMesh = AssetLoader::loadMesh("cube.obj");
-    std::vector<Entity> scene = { {cubeMesh, {-1.0f, 0, -5.0f}, 1.5f}, {cubeMesh, {1.0f, 0, -5.0f}, 2.5f} };
+    std::vector<Entity> scene;
+    scene.push_back(Entity(cubeMesh, {0.0f, 0.0f, -5.0f}, 1.0f));
 
-    // Basic shader - no lighting calculations to avoid memory mismatches
     Shader myShader(R"(#version 330 core
         layout(location=0) in vec3 aPos; layout(location=1) in vec2 aTex;
         uniform mat4 model; uniform mat4 view; uniform mat4 projection;
@@ -35,11 +38,22 @@ int main() {
     if(d) { glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, d); glGenerateMipmap(GL_TEXTURE_2D); stbi_image_free(d); }
 
     Camera cam;
+    bool spacePressed = false; // Debounce for spawning
+
     while (!glfwWindowShouldClose(window)) {
         if (Input::keys[GLFW_KEY_W]) cam.Move(0, -0.05f);
         if (Input::keys[GLFW_KEY_S]) cam.Move(0, 0.05f);
         if (Input::keys[GLFW_KEY_A]) cam.Move(-0.05f, 0);
         if (Input::keys[GLFW_KEY_D]) cam.Move(0.05f, 0);
+
+        // Scene Editor: Spawn object
+        if (Input::keys[GLFW_KEY_SPACE] && !spacePressed) {
+            float rx = ((float)rand()/(float)RAND_MAX) * 4.0f - 2.0f;
+            float ry = ((float)rand()/(float)RAND_MAX) * 4.0f - 2.0f;
+            scene.push_back(Entity(cubeMesh, {rx, ry, -5.0f}, 1.0f + (float)(rand()%5)));
+            spacePressed = true;
+        }
+        if (!Input::keys[GLFW_KEY_SPACE]) spacePressed = false;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         myShader.use();
